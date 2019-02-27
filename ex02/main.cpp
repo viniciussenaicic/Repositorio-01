ebc3466d15f61851935b7755a2da5faf7a41faf2
viniciussenaicic <47800028+viniciussenaicic@users.noreply.github.com>
2019-02-26 22:02:54 -0300

    Add files via upload

---
+++ ex02/main.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <iostream>
+
+int main()
+	{
+	//system ("cls");
+	
+	int A = 0;
+	
+	printf (" Informe um valor A inteiro: \n");
+	
+	scanf ("%i", &A);
+	
+	printf ("O valor de A eh: %i \n", A);
+	system ("PAUSE");	
+	return 0;
+	}
